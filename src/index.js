75b0029dec30c95e1fdbd3dc6c69595d68d1c132
mayurlilhare <mayurlilhare00@gmail.com>
2024-12-17 18:15:54 +0530

    removed all comments

--- src/index.js
+++ src/index.js
@@ -5,15 +5,7 @@ import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <App /> //lorem ipsum 
-//     Enumerating objects: 7, done.
-// Counting objects: 100% (7/7), done.
-// Delta compression using up to 32 threads
-// Compressing objects: 100% (4/4), done.
-// Writing objects: 100% (4/4), 410 bytes | 410.00 KiB/s, done.
-// Total 4 (delta 3), reused 0 (delta 0), pack-reused 0 (from 0)
-// remote: Resolving deltas: 100% (3/3), completed with 3 local objects.
-// To https://github.com/mayurlilhare/redux-toolkit.git
+    <App /> 
 );
 
 
