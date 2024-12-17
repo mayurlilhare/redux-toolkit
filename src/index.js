6b51bf06067c045bb3836e7b4b7f6cb6c5eb88c9
mayurlilhare <mayurlilhare00@gmail.com>
2024-12-17 18:04:48 +0530

    my changes

--- src/index.js
+++ src/index.js
@@ -5,7 +5,8 @@ import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <App /> 
+    <App /> /// changes in mayurl
+    //needs to check if merger is successful.
 );
 
 
